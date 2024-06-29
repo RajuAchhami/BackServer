39de4af5f60b1ac3e7e7020fa27f4ea5c05dfeb7
Raju Achhami <rajachhami55@gamil.com>
2024-06-29 07:58:29 +0545

    ch

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ import mongoose from "mongoose";
 
 const port = 5000;
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://rajachhami55:Raju12345@cluster0.s3emclp.mongodb.net/demo"
+  )
+  .then((val) => {
+    app.listen(port, () => {
+      console.log("Server Started");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(cors());
 app.use(bodyParser.json());
 
-main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose
-    .connect(
-      "mongodb+srv://rajachhami55:Raju12345@cluster0.s3emclp.mongodb.net/demo"
-    )
-    .then((val) => {
-      app.listen(port, () => {
-        console.log("Server Started");
-      });
-    });
-}
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    msg: "welcome to my server",
+  });
+});
 
 app.use("/api", productRoutes);
 app.use("/api", navRoutes);
